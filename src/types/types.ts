3f77440c627763484cdde8e4c666ad6d47d4b185
Nguyen Trung Dat <datnguyentrung.ptit@gmail.com>
2026-02-23 14:37:52 +0700

    feat: initialize React app with Vite, add global styles, and set up API utilities
    - Created main application structure with App.tsx and App.css
    - Added axios instance for API requests with interceptors for request and response handling
    - Included global styles and variables using SCSS
    - Implemented utility functions for date formatting
    - Defined TypeScript types for API responses and application data structures
    - Configured Vite with React and Tailwind CSS support
    - Set up TypeScript configuration for app and node environments

---
+++ src/types/types.ts
@@ -0,0 +1,72 @@
+export type SeatType = "STANDARD" | "VIP" | "COUPLE";
+export type SeatState = "AVAILABLE" | "BOOKED" | "SELECTED" | "UNAVAILABLE";
+export type AgeRating = "P" | "C13" | "C16" | "C18";
+export type RoomType = "2D" | "3D" | "IMAX";
+export type ShowtimeStatus = "OPENING" | "SOLD_OUT" | "CANCELLED";
+export type TicketStatus = "HOLD" | "PAID" | "USED" | "CANCELLED";
+
+export interface BookSeatsRequest{
+  showtimeId: string;
+  seatIds: number[];
+}
+
+export interface TicketResponse{
+  ticketId: string;
+  bookingId: string;
+  showtimeId: string;
+  seatId: number;
+
+  ticketCode: string;
+  price: number;
+  createdAt: Date | string;
+  status: TicketStatus;
+  checkInTime?: Date | string;
+}
+
+export interface SeatResponse {
+  seatId: number;
+  roomId: number;
+
+  rowLabel: string;
+  seatNumber: number;
+  gridRow: number;
+  gridCol: number;
+
+  isActive: boolean;
+
+  seatType: SeatType;
+  seatState?: SeatState;
+}
+
+export interface MovieResponse {
+  movieId: string;
+  title: string;
+  durationMinutes: number;
+  posterUrl: string;
+  description: string;
+  releaseDate: Date | string;
+  ageRating: AgeRating;
+}
+
+export interface RoomResponse {
+  roomId: number;
+  roomName: string;
+  totalRows: number;
+  totalCols: number;
+  type: RoomType;
+}
+
+export interface ShowtimeResponse {
+  showtimeId: string;
+  movieResponse: MovieResponse;
+  roomResponse: RoomResponse;
+  startTime: Date | string;
+  endTime: Date | string;
+
+  ticketPrice: number;
+  status: ShowtimeStatus;
+}
+
+export interface ShowTimeWithSeatsResponse extends ShowtimeResponse {
+  selectedSeats: SeatResponse[];
+}
